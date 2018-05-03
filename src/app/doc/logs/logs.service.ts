0c97149c3f5b019454fd6edf1c5950dc0b79f6c0
Cedric Enclos <cedric.enclos@loria.fr>
2018-05-03 11:01:54 +0200

    feat(logs): Add RabitMq class for sending logs to a message queue

--- src/app/doc/logs/logs.service.ts
+++ src/app/doc/logs/logs.service.ts
@@ -1,33 +1,43 @@
 import { Injectable } from '@angular/core'
 import { Database } from './Database'
 import { IndexdbDatabase } from './IndexdbDatabase'
+import { RabbitMq } from './RabbitMq'
 
 @Injectable()
 export class LogsService {
-  private db: Database
+
+  private dbLocal: Database
+  private dbDistante: RabbitMq
+
+  private docKey: string
   private displayLogs: boolean
 
-  constructor(databaseName: string) {
-    this.db = new IndexdbDatabase()
-    this.db.init(databaseName)
+  constructor (docKey: string) {
+    this.docKey = docKey
     this.displayLogs = false
+
+    // Initialize the local DB
+    this.dbLocal = new IndexdbDatabase()
+    this.dbLocal.init('muteLogs-' + this.docKey)
+
+    // Initialize the distant DB
+    this.dbDistante = new RabbitMq(this.docKey)
   }
 
-  log(obj: object) {
-    this.db.store(obj)
+  log (obj: object) {
     if (this.displayLogs) {
       console.log('[LOGS]', obj)
     }
+
+    this.dbLocal.store(obj)
+    this.dbDistante.send(obj)
   }
 
   getLogs(): Promise<object[]> {
     return new Promise((resolve, reject) => {
-      this.db
-        .get()
-        .then((obj: object) => {
-          resolve(obj as object[])
-        })
-        .catch((err) => reject(err))
+      this.dbLocal.get().then((obj: object) => {
+        resolve(obj as object[])
+      }).catch((err) => reject(err))
     })
   }
 
